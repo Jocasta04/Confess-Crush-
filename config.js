431f2819f180de12eab8ca76c299a1ac3f353b7f
Laius1108 <93584015+Laius1108@users.noreply.github.com>
2021-11-02 18:35:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Laius1108",
+    introTitle: 'Thùy Linh ơi',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnIntro: '^^Ok anh^^',
+    title: 'Thùy Linh có iu Thành Đạt không nà',
+    desc: 'Hãy trả lời anh nhé ',
+    btnYes: 'Iu nhiều <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Lí do gì khiến em chọn anh <3',
+    btnReply: 'Gửi cho Thành Đạt <3',
+    reply: 'Vì Đạt đẹp trai vl <33333333',
+    mess: 'Anh biết ngay mà',
+    messDesc: 'Chủ nhật anh qua đón nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/cmthanhdat'.
 }

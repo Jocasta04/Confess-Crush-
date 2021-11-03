2b410d8bb5c2dec0d4b7053ae474558be750a5a4
Jocasta04 <93664517+Jocasta04@users.noreply.github.com>
2021-11-03 22:01:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,16 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Thùy Linh ơi',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Thùy Linh có iu Thành Đạt không nà?',
-    desc: 'Hãy trả lời anh ',
-    btnYes: 'Iu nhiều lắm<33',
-    btnNo: 'Không nha :3',
-    question: 'Lí do tại sao em lại chọn anh <3',
-    btnReply: 'Gửi cho Đạt <3',
-    reply: 'Vì anh đẹp trai vl <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Chủ nhật nay , anh qua đón nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://m.me/cmthanhdat'
+    introTitle: 'Duyn ưiiiiii!!!',
+    introDesc: `Trên thế giới có hơn 7 tỉ người và xấp xỉ 8 tỉ người
+    Tại sao Duyn vẫn chưa có người yêu?!!!',
+    btnIntro: 'Iuiu',
+    title: 'Duyn có iu mình hơmmmm',
+    desc: 'rely meeee ',
+    btnYes: 'iu thương',
+    btnNo: 'ghét',
+    question: 'Tại sao Duyn chưa có người yêu?',
+    btnReply: 'Gửi cho Linhhhh',
+    reply: 'Vì Duyn mặc áo Mickey',
+    mess: 'hahahahhaha',
+    messLink: 'https://m.me/linhxinhdep'
 }

ab65a8a9a2e3df58223ef45a33f83749a204cdd7
Jocasta04 <93664517+Jocasta04@users.noreply.github.com>
2021-11-03 22:06:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,7 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Thùy Linh ơi',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'bọn mày ơi',
+    introDesc: `đồ ế hahahahha`,
     btnIntro: '^^HiHi^^',
-    title: 'Thùy Linh có iu Thành Đạt không nà?',
-    desc: 'Hãy trả lời anh ',
-    btnYes: 'Iu nhiều lắm<33',
-    btnNo: 'Không nha :3',
-    question: 'Lí do tại sao em lại chọn anh <3',
-    btnReply: 'Gửi cho Đạt <3',
-    reply: 'Vì anh đẹp trai vl <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Chủ nhật nay , anh qua đón nha.',
-    btnAccept: 'Okiiiii lun <3',
     messLink: 'https://m.me/cmthanhdat'
 }
